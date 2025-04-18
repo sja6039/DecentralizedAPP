cd92c7f36d6796db77d9613b5c5ee559e658a59c
Samuel Adams <sja6039@psu.edu>
2025-04-18 18:08:28 -0400

    touches

--- components/Dashboard/Navbar.js
+++ components/Dashboard/Navbar.js
@@ -19,7 +19,6 @@ const Navbar = () => {
   };
   
   const handleConnectWallet = () => {
-    // This would integrate with MetaMask or other wallet providers
     setIsWalletConnected(!isWalletConnected);
   };
 
